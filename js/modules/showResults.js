2857f9541eb124bce896af775025f64819dfd43a
Przemek <przemyslaw.pajka94@gmail.com>
2020-01-31 17:26:07 +0100

    remove consoles

--- js/modules/showResults.js
+++ js/modules/showResults.js
@@ -1,6 +1,5 @@
 import gameResults from './gameResults.js'
 export default function showResult({result,currentGameState}) {  
-    console.log({result,currentGameState});  
     switch(result) {
         case 'nextRound': gameResults(currentGameState).getNextRound(); break;
         case 'looseGame': gameResults(currentGameState).looseGame(); break;
